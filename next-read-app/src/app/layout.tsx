6a48f5094cdd3583d2a4c28bb21a1a2da2aa7705
Iva Pleic <ipleic46@gmail.com>
2024-11-07 21:44:53 +0100

    try deploy 2

--- next-read-app/src/app/layout.tsx
+++ next-read-app/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import  Navbar  from "./components/Navbar/NavBar";
 import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
