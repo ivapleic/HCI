c81b99f60183395b3d2fe68adc85177a41d74b4a
Iva Pleic <ipleic46@gmail.com>
2024-11-07 21:58:07 +0100

    added favicon

--- next-read-app/src/app/layout.tsx
+++ next-read-app/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel='icon' href='/assets//Favicon.png' />
+      <link rel='icon' href='/assets//favicon.ico' />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
