a77f2a6007aff3f226f236363db57f941ed58f9f
Iva Pleic <ipleic46@gmail.com>
2024-11-11 12:03:30 +0100

    Successfuly connected Contentful with next-js app

--- next-read-app/src/app/auth/register/page.tsx
+++ next-read-app/src/app/auth/register/page.tsx
@@ -1,11 +1,36 @@
+"use client"
+import React, { useState } from "react";
 import Image from "next/image";
 
 export default function RegisterPage() {
+  // Držimo sve korisničke podatke u jednom objektu
+  const [userInput, setUserInput] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  // Funkcija koja ažurira stanje objekta
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Ovde možete slati podatke na backend, na primer:
+    console.log(userInput);
+  };
+
   return (
     <div className="flex min-h-screen">
       {/* Lijevi dio - forma */}
       <div className="w-1/2 h-full flex items-center justify-center p-10">
-        <form className="w-full max-w-sm">
+        <form className="w-full max-w-sm" onSubmit={handleSubmit}>
           <h1 className="text-3xl font-bold mb-6">Registration</h1>
 
           <div className="mb-4">
@@ -17,10 +42,13 @@ export default function RegisterPage() {
             </label>
             <input
               id="fullName"
+              name="fullName" // Koristimo `name` atribut za praćenje polja
               type="text"
               placeholder="Full Name"
               className="border border-gray-300 rounded-lg p-2 w-full bg-[#F9F3EE] focus:border-[#8C6954] focus:outline-none"
               required
+              value={userInput.fullName} // Povezivanje sa stanjem
+              onChange={handleInputChange} // Funkcija koja ažurira stanje
             />
           </div>
 
@@ -33,10 +61,13 @@ export default function RegisterPage() {
             </label>
             <input
               id="email"
+              name="email"
               type="email"
               placeholder="Email"
               className="border border-gray-300 rounded-lg p-2 w-full bg-[#F9F3EE] focus:border-[#8C6954] focus:outline-none"
               required
+              value={userInput.email} // Povezivanje sa stanjem
+              onChange={handleInputChange} // Funkcija koja ažurira stanje
             />
           </div>
 
@@ -49,10 +80,13 @@ export default function RegisterPage() {
             </label>
             <input
               id="password"
+              name="password"
               type="password"
               placeholder="Password"
               className="border border-gray-300 rounded-lg p-2 w-full bg-[#F9F3EE] focus:border-[#8C6954] focus:outline-none"
               required
+              value={userInput.password} // Povezivanje sa stanjem
+              onChange={handleInputChange} // Funkcija koja ažurira stanje
             />
           </div>
 
--- next-read-app/src/app/page.tsx
+++ next-read-app/src/app/page.tsx
@@ -1,7 +1,44 @@
-export default function Home() {
+"use client";
+
+import React, { useEffect, useState } from "react";
+import contentfulService from "./lib/ContenfulClient";
+import { Entry } from "contentful";
+
+const HomePage = () => {
+  const [genres, setGenres] = useState<Entry<any>[]>([]);
+  const [users, setUsers] = useState<Entry<any>[]>([]);
+
+  useEffect(() => {
+    const fetchGenres = async () => {
+      const fetchedGenres = await contentfulService.getAllGenres();
+      setGenres(fetchedGenres);
+      console.log(fetchedGenres);
+    };
+
+    const fetchUsers= async () => {
+      const fetchedUsers = await contentfulService.getAllUsers();
+      setUsers(fetchedUsers);
+      console.log(fetchedUsers);
+    };
+
+
+    fetchGenres();
+    fetchUsers();
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center p-10">
-      <h1 className="text-6xl font-extrabold tracking-tight">Home page</h1>
+      <h1 className="text-6xl font-extrabold tracking-tight">Home Page</h1>
+      <p>List of genres:</p>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre.sys.id}>
+             <h2>{genre.fields.name}</h2> 
+          </li>
+        ))}
+      </ul>
     </main>
   );
-}
+};
+
+export default HomePage;
