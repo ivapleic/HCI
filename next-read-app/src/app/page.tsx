e163cfd49aedc9b8bf69bd0dc40652045a1c2e89
Iva Pleic <ipleic46@gmail.com>
2024-12-07 00:39:57 +0100

    try deploy 3

--- next-read-app/src/app/page.tsx
+++ next-read-app/src/app/page.tsx
@@ -1,45 +1,15 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-//import contentfulService from "./lib/ContenfulClient";
-import { Entry } from "contentful";
 
 const HomePage = () => {
 
-  /*
-  const [genres, setGenres] = useState<Entry<any>[]>([]);
-  const [users, setUsers] = useState<Entry<any>[]>([]);
-
-  useEffect(() => {
-    const fetchGenres = async () => {
-      const fetchedGenres = await contentfulService.getAllGenres();
-      setGenres(fetchedGenres);
-      console.log(fetchedGenres);
-    };
-
-    const fetchUsers= async () => {
-      const fetchedUsers = await contentfulService.getAllUsers();
-      setUsers(fetchedUsers);
-      console.log(fetchedUsers);
-    };
-
-
-    fetchGenres();
-    fetchUsers();
-  }, []);
-  */
 
   return (
     <main className="flex min-h-screen flex-col items-center p-10">
       <h1 className="text-6xl font-extrabold tracking-tight">Home Page</h1>
       <p>List of genres:</p>
-      <ul>
-        {/* {genres.map((genre) => (
-          <li key={genre.sys.id}>
-     
-          </li>
-        ))} */}
-      </ul>
+    
     </main>
   );
 };
